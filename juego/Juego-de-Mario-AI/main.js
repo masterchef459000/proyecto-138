450e40a1198dbae11bfb52d401cdb951f8c74328
masterchef459000 <107654477+masterchef459000@users.noreply.github.com>
2024-03-09 09:42:01 -0600

    Add files via upload

---
+++ juego/Juego-de-Mario-AI/main.js
@@ -0,0 +1,52 @@
+noseX="";
+noseY="";
+GameStatus="";
+function game(){
+	console.log("nosex = "+noseX+", nosey = "+noseY);
+}
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump=loadSound("jump.wav");
+	mario_coin=loadSound("coin.wav");
+	mario_gameover=loadSound("gameover.wav");
+	mario_die=loadSound("mariodie.wav");
+	mario_kick=loadSound("kiks.wav");
+
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas;')
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+	function modelLoaded() {
+		console.log('¡Modelo cargado!');
+	  }
+	  
+	  function gotPoses(results)
+	  {
+		if(results.length > 0)
+		{
+		  noseX = results[0].pose.nose.x;
+		  noseY = results[0].pose.nose.y;
+		  console.log("noseX = " + noseX +", noseY = " + noseY);
+		}
+	  }
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
